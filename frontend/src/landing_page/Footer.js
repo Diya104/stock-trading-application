8f77b694edafc36904d52d2f5663d0510c818ede
Diya104 <diyabansal104@gmail.com>
2025-06-24 19:04:00 +0530

    completed homepage + set up router

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React from "react";
 
- function Footer() {
-    return ( 
-        <h1>Footer</h1>
-     );
- }
- 
- export default Footer;
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: "rgb(250, 250, 250)", paddingBottom: "2rem" }}>
+      <div className="container border-top mt-5">
+        <div className="row mt-5">
+          <div className="col">
+            <img src="media/images/logo.svg" style={{ width: "50%" }} alt="Logo" />
+            <p>
+              &copy; 2010 - 2024, Not Zerodha Broking Ltd. All rights reserved.
+            </p>
+          </div>
+          <div className="col">
+            <p>Company</p>
+            <a href="">About</a>
+            <br />
+            <a href="">Products</a>
+            <br />
+            <a href="">Pricing</a>
+            <br />
+          </div>
+          <div className="col">
+            <p>Support</p>
+            <a href="">Contact</a>
+            <br />
+            <a href="">Support portal</a>
+            <br />
+          </div>
+          <div className="col">
+            <p>Account</p>
+            <a href="">Open an account</a>
+            <br />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
