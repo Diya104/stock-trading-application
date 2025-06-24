08b0ade60675e1224a9b16e02ae05a0e40edfa8b
Diya104 <diyabansal104@gmail.com>
2025-06-24 19:11:55 +0530

    finished products page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,11 @@ import "./index.css";
 import HomePage from "./landing_page/home/HomePage";
 // import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage";
-// import ProductPage from "./landing_page/products/ProductsPage";
+import ProductPage from "./landing_page/products/ProductsPage";
 // import PricingPage from "./landing_page/pricing/PricingPage";
 // import SupportPage from "./landing_page/support/SupportPage";
 
-// import NotFound from "./landing_page/NotFound";
+import NotFound from "./landing_page/NotFound";
 import Navbar from "./landing_page/Navbar";
 import Footer from "./landing_page/Footer";
 
@@ -22,10 +22,10 @@ root.render(
       <Route path="/" element={<HomePage />} />
       {/* <Route path="/signup" element={<Signup />} /> */}
       <Route path="/about" element={<AboutPage />} />
-      {/* <Route path="/product" element={<ProductPage />} />
-      <Route path="/pricing" element={<PricingPage />} />
-      <Route path="/support" element={<SupportPage />} />
-      <Route path="*" element={<NotFound />} /> */}
+      <Route path="/product" element={<ProductPage />} />
+      {/*<Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} */}
+      <Route path="*" element={<NotFound />} /> 
     </Routes>
     <Footer />
   </BrowserRouter>
